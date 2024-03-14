73c5d83e17e3e5ac0414daf447829397ec6a12e0
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-03-14 17:41:52 +0900

    Spring Day 02
    MVC Pattern에 대한 이해.
    컨트롤러 & 모델 & 뷰의 상호작용

---
+++ src/main/java/prjoect/firstproject/controller/FirstController.java
@@ -0,0 +1,18 @@
+package prjoect.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi") // URL 연결 요청 == @GetMapping
+    public String niceToMeetYou(Model model){ //Model을 사용하기 위하여 파라미터로 Model model 을 넣어주었다.
+        model.addAttribute("username", "철우");
+        // model을 통해 변수를 등록할 수 있다. & 변수 등록 addAttribute()
+        // 철우라는 값을 username 이름에 연결 시켜 model이라는 객체를 통해 보내준다.
+
+        return "greetings"; // templates/greetings.mustache를 연결하여 브라우저로 전송하게 한다.
+        //return 값으로 보여줄 페이지의 이름을 넣어준다.
+    }
+}
