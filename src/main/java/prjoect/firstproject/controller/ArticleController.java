f48779a1d38c364cbadd9bab6d71b5fa13e37ab7
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-05-09 14:21:41 +0900

    Spring Day 13 - 데이터 삭제하기
    DB와 상호작용하여 데이터가 존재한다면, 데이터를 삭제해보자.

--- src/main/java/prjoect/firstproject/controller/ArticleController.java
+++ src/main/java/prjoect/firstproject/controller/ArticleController.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import prjoect.firstproject.dto.ArticleForm;
 import prjoect.firstproject.entity.Article;
 import prjoect.firstproject.repository.ArticleRepository;
@@ -109,6 +111,27 @@ public class ArticleController {
 
         // 3 : 수행 결과 페이지를 리다이렉트 한다.
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제 요청이 입력되었습니다.");
+
+        // 1: 삭제 대상을 가져온다!
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        // 2: 대상을 삭제한다 !
+        if (target != null){
+            articleRepository.delete(target); // target이 값이 null이 아니면(데이터가 존재한다면 삭제한다.)
+            rttr.addFlashAttribute("msg", "삭제가 완료되었습니다 !");
+
+        }
+
+        // 3: 결과 페이지로 리다이렉트 한다!
+
+        return "redirect:/articles";
+        // 리다이렉트 될 페이지는 index/mustache이다.
     }
 }
 // 사용자가 브라우저를 통해서 데이터를 요청하고, 요청URL을 컨트롤러가 받고, 받아진 URL을 찾고자 하는 정보를 Repository에 전달함.
