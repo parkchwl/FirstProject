6dbdfdfd4dec95bc4639924dce4c45001400fa34
parkchwl <93250361+parkchwl@users.noreply.github.com>
2024-05-17 02:47:10 +0900

    Spring Day 16 - Service & Transaction
    Rest API 구현에 서비스 계층 추가하기

--- src/main/java/prjoect/firstproject/api/ArticleApiController.java
+++ src/main/java/prjoect/firstproject/api/ArticleApiController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import prjoect.firstproject.dto.ArticleForm;
 import prjoect.firstproject.entity.Article;
 import prjoect.firstproject.repository.ArticleRepository;
+import prjoect.firstproject.service.ArticleService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,57 +17,50 @@ import java.util.List;
 @RestController // Rest API 용 컨트롤러 ! => 데이터(JSON)를 반환한다.
 public class ArticleApiController {
     @Autowired // DI = 외부에서 가져온다. 의존선 주입 !
-    private ArticleRepository articleRepository;
+    private ArticleService articleService;
 
     // GET
     @GetMapping("/api/articles")
-    public ArrayList<Article> index(){
-        return articleRepository.findAll();
+    public List<Article> index(){
+        return articleService.index();
     }
 
     @GetMapping("/api/articles/{id}")
-    public Article index(@PathVariable Long id){
-        return articleRepository.findById(id).orElse(null);
+    public Article show(@PathVariable Long id){
+        return articleService.show(id);
     }
 
-    @PostMapping("api/articles")
-    public Article create(@RequestBody ArticleForm dto){
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
+    // POST
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto){ //Resquset Body부분을 ArticleForm dto 로 오게 하는 Annotation
+        Article created = articleService.create(dto);
+        return (created != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
-    // PATCH
     @PatchMapping("/api/articles/{id}")
-    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody ArticleForm dto){
-        // PATCH -> 수정은 다음과 같아야 한다.
-        // 1: 수정용 엔티티 생성
-        Article article = dto.toEntity();
-        log.info("id: {}, article: {}", id, article.toString());
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
 
-        // 2: 대상 엔티티를 조회
-        Article target = articleRepository.findById(id).orElse(null);
-
-        // 3: 잘못된 요청 처리(대상이 없거나, id가 다른 경우
-        if (target ==null || id != article.getId()){
-            log.info("잘못된 요청! id: {},article: {}", id, article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-
-        // 4: 업데이트 및 정상 응답(200)
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
+        Article updated = articleService.update(id, dto);
+        return (updated != null) ? ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
-
-    // DELETE
+    //DELETE
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Article> delete(@PathVariable Long id){
-        Article target = articleRepository.findById(id).orElse(null);
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.OK).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 
-        if (target == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        articleRepository.delete(target);
-        return ResponseEntity.status(HttpStatus.OK).build();
+    // 트랜잭션이 실패하면 롤백한다.
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos){
+        List<Article> createdList = articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
